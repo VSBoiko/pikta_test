913fd27336aa4010c583ca1ac3128c055b3ee3e7
vlad boiko <mail@vladboiko.ru>
2022-06-16 19:38:10 +0300

    task 3 - added method get_clients_amount() to class PiktaDb

--- task_3/PiktaDb.py
+++ task_3/PiktaDb.py
@@ -8,7 +8,7 @@ class PiktaDb:
 
     def init_db(self, add_data: bool = False):
         """
-        Метод создает базу данныx и добавляет в нее таблицы
+        Создает базу данныx и добавляет в нее таблицы
 
         :param add_data: заполнить или нет таблицы данными
         """
@@ -17,6 +17,47 @@ class PiktaDb:
         if add_data:
             self.__add_data()
         self.__connection_close()
+
+    # Метод, который возвращает результаты запроса: список клиентов с общей суммой их покупки
+
+    def get_clients_amount(self) -> list:
+        """
+        Возвращает список клиентов с общей суммой их покупки
+
+        :return: список клиентов с общей суммой их покупки
+        Например:
+        [
+            {
+                "client_name": "Иван",
+                "amount": 100.10
+            },
+            {
+                "client_name": "Петр",
+                "amount": 200.00
+            },
+        ]
+        """
+        self.__connection_open()
+        cur = self.db.cursor()
+        select_sql = """SELECT
+            cl.client_name AS client_name,
+            SUM(pr.product_price) AS amount
+        FROM clients cl
+            JOIN orders ord ON cl.client_id = ord.client_id
+            JOIN products pr ON ord.product_id = pr.product_id
+        group by client_name
+        """
+        cur.execute(select_sql)
+        results_sql = cur.fetchall()
+        result = []
+        for row in results_sql:
+            result.append({
+                "client_name": row["client_name"],
+                "amount": round(row["amount"], 2)
+            })
+        self.__connection_close()
+
+        return result
 
     def __add_data(self):
         clients = [
@@ -128,6 +169,11 @@ class PiktaDb:
         cur = self.db.cursor()
         cur.execute(insert_sql, (name, price))
         return cur.lastrowid
+
+    def __get_db(self):
+        if self.db is None:
+            self.init_db()
+        return self.db
 
     def __set_db(self, db):
         self.db = db
--- task_3/test.py
+++ task_3/test.py
@@ -3,6 +3,13 @@ from PiktaDb import PiktaDb
 
 p = PiktaDb(db_path="pikta_test.db")
 
+
 # Метод, который создает базу данных, таблицы со связями и наполняет таблицы данными
 # p.init_db(add_data=True)          # наполняет таблицы данными
 p.init_db()             # не наполняет таблицы данными
+
+
+# Метод, который возвращает результаты запроса: список клиентов с общей суммой их покупки
+clients_amount = p.get_clients_amount()
+for amount in clients_amount:
+    print(f"{amount['client_name']} - {amount['amount']}")
